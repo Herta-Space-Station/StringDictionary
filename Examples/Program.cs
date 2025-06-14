33e1a90cdb5448bca20eb065150d0a2812659f9a
Molth Nevin <qq2201591870@163.com>
2025-06-15 00:59:25 +0800

    Update Program.cs

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -9,13 +9,22 @@ namespace Examples
         private static void Main()
         {
             var dictionary = new StringDictionary<TestResource>();
-            var stringBuilder = ZString.CreateStringBuilder();
+            Example(dictionary);
+        }
+
+        public static void Example(StringDictionary<TestResource> dictionary)
+        {
+            using var stringBuilder = ZString.CreateStringBuilder();
             stringBuilder.Append(100);
             ref var resource = ref dictionary.GetValueRefOrAddDefault(stringBuilder.AsSpan(), out var exists);
             Console.WriteLine(resource == null);
             if (!exists)
                 resource = new TestResource { Value = 100 };
+
             Console.WriteLine(resource!.Value);
+
+            //TODO: never use `ToString()`
+
             Console.WriteLine(dictionary[stringBuilder.ToString()].Value);
         }
     }
